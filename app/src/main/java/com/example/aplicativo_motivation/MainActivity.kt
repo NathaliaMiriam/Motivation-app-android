cc1ce7e215ccf46d7b27c1e272699ca53865b747
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-03-08 19:41:10 -0300

    stylizes MainActivity and includes onclick event

--- app/src/main/java/com/example/aplicativo_motivation/MainActivity.kt
+++ app/src/main/java/com/example/aplicativo_motivation/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.aplicativo_motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.aplicativo_motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // esconde a barra roxa de navegação
+        supportActionBar?.hide()
+
+        // evento de clique
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
     }
 }
