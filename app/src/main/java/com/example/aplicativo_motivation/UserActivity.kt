922ec32f98b713c73bb604e4ee8c3a5b5c7a2b08
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-03-09 18:12:49 -0300

    starts new screen and the validation and navigation of the Activities

---
+++ app/src/main/java/com/example/aplicativo_motivation/UserActivity.kt
@@ -0,0 +1,39 @@
+package com.example.aplicativo_motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.aplicativo_motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
